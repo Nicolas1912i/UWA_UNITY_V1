fbae9a8677b901e09be6111cecb64de4690c71db
Nicolas Arias <nicolas.arias@newshore.es>
2023-06-11 11:29:16 -0500

    Added solar system and rotation controls attempt

---
+++ Assets/Scripts/mainMenu/GenerateAvaliableProfiles.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateAvaliableProfiles : MonoBehaviour
+{
+    // This script will instance the buttons to access the created profiles stored in memory
+}
