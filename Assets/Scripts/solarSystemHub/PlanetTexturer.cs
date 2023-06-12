a50a10cf4b22db63c4008f8ac23fc3bacc12fe93
Nicolas Arias <nicolas.arias@newshore.es>
2023-06-11 20:39:44 -0500

    I don't have idea for a comment

---
+++ Assets/Scripts/solarSystemHub/PlanetTexturer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlanetTexturer : MonoBehaviour
+{
+    [SerializeField] private Texture2D[] frames;
+    [SerializeField] private float fps = 1.0f;
+
+    private Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        int index = (int)(Time.time * fps);
+        index = index % frames.Length;
+        mat.mainTexture = frames[index];
+    }
+}
